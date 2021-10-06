e7991568a54cc8655727b33ecb7a656d688fdae5
javier.gonzalez <javier.gonzalez@piksel.com>
2021-10-06 12:42:38 +0200

    Initial commit

---
+++ lambdas/endpoints/getWorks.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const AWS = require("aws-sdk");
+
+const dynamoDB = new AWS.DynamoDB({
+  region: "us-east-1",
+  apiVersion: "2021-10-05",
+});
+
+module.exports.handler = (event, context, cb) => {
+  const params = {
+    TableName: "portfolio",
+  };
+
+  dynamoDB.scan(params, (err, data) => {
+    if (err) {
+      console.log(err);
+      cb(err);
+    } else {
+      const unmarshalledData = data.Items.map((el) => {
+        return AWS.DynamoDB.Converter.unmarshall(el);
+      });
+
+      const response = {
+        isBase64Encoded: false,
+        statusCode: 200,
+        body: JSON.stringify(unmarshalledData),
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+      };
+
+      cb(null, response);
+    }
+  });
+};
