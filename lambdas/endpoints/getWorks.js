485af9a5009506db292a9120fd9e733816fcd2f8
javier.gonzalez <javier.gonzalez@piksel.com>
2021-10-11 21:30:41 +0200

    add headers

--- lambdas/endpoints/getWorks.js
+++ lambdas/endpoints/getWorks.js
@@ -28,6 +28,9 @@ module.exports.handler = (event, context, cb) => {
         headers: {
           "Access-Control-Allow-Origin": "*",
           "Access-Control-Allow-Credentials": true,
+          "Access-Control-Allow-Headers":
+            "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+          "Access-Control-Allow-Methods": "GET, OPTIONS",
         },
       };
 
