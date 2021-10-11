2c955ed7fa258dfee4e7f29c2182a2d30d60b3a3
javier.gonzalez <javier.gonzalez@piksel.com>
2021-10-11 22:34:04 +0200

    Update getWorks.js

--- lambdas/endpoints/getWorks.js
+++ lambdas/endpoints/getWorks.js
@@ -28,9 +28,6 @@ module.exports.handler = (event, context, cb) => {
         headers: {
           "Access-Control-Allow-Origin": "*",
           "Access-Control-Allow-Credentials": true,
-          "Access-Control-Allow-Headers":
-            "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
-          "Access-Control-Allow-Methods": "GET, OPTIONS",
         },
       };
 
